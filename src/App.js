6eedc288186d17766f3b36d48c4d81d44f83dbf5
Tasos Stefanidis <tasos.stefanidis@centtrip.com>
2024-11-16 21:01:22 +0000

    adding 2 seconds delay on page2 to see the fallback
    Signed-off-by: Tasos Stefanidis <tasos.stefanidis@centtrip.com>

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 import Page1 from './Components/Page1';
 
 
-const Page2Component = React.lazy(() => import('./Components/Page2'));
+// const Page2Component = React.lazy(() => import('./Components/Page2'));
+
+// Simulate a delay for Page2Component
+const Page2Component = React.lazy(() => new Promise(resolve => {
+    setTimeout(() => resolve(import('./Components/Page2')), 2000);
+}));
+
 const Page3Component = React.lazy(() => import('./Components/Page3'));
 
 class App extends Component {
@@ -20,21 +26,21 @@ class App extends Component {
 
 
     render() {
+        let componentToRender;
+
         if (this.state.route === 'page1') {
-            return <Page1 onRouteChange={this.onRouteChange}/>
+            componentToRender = <Page1 onRouteChange={this.onRouteChange}/>;
         } else if (this.state.route === 'page2') {
-            return (
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Page2Component onRouteChange={this.onRouteChange}/>
-                </Suspense>
-            );
+            componentToRender = (<Suspense fallback={<div>Loading...</div>}>
+                <Page2Component onRouteChange={this.onRouteChange}/>
+            </Suspense>);
         } else {
-            return (
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Page3Component onRouteChange={this.onRouteChange}/>
-                </Suspense>
-            );
+            componentToRender = (<Suspense fallback={<div>Loading...</div>}>
+                <Page3Component onRouteChange={this.onRouteChange}/>
+            </Suspense>);
         }
+
+        return componentToRender;
     }
 }
 
